fdac70af9c26c5f87714ecccfbbdd2e74ea57098
undg <und3rdg@gmail.com>
2023-02-01 18:36:24 +0000

    First iteration for interacting with OS.

--- sys/landscape.go
+++ sys/landscape.go
@@ -1,5 +1,11 @@
 package sys
 
+import (
+	"log"
+	"os/exec"
+	"strconv"
+)
+
 // SCREEN="eDP"
 // TRANSFORMATION="Coordinate Transformation Matrix"
 
@@ -9,7 +15,27 @@ package sys
 // xinput set-prop $i --type=float "$TRANSFORMATION" 1 0 0 0 1 0 0 0 1
 
 func Landscape() string {
-	message := "Landscape: placeholder"
 
+	screen := "eDP"
+	devicesIds := [6]int{10, 12, 13, 16, 20, 21}
+	message := "Landscape: placeholder: " + screen
+
+
+	cmdXrandr := exec.Command("xrandr --output", screen, "--rotate left")
+	errXrandr := cmdXrandr.Run()
+	if errXrandr != nil {  
+		log.Fatal(errXrandr)
+	}
+	
+
+	for _, deviceId := range devicesIds {
+		message += strconv.Itoa(deviceId)	
+
+		cmdXinput := exec.Command("xinput set-prop", strconv.Itoa(deviceId), "--type=foat Coordinate Transformation Matrix", "1 0 0 0 1 0 0 0 1")
+		errXinput := cmdXinput.Run()
+		if errXinput != nil {
+			log.Fatal(errXinput)
+		}
+	}
 	return message
 }
